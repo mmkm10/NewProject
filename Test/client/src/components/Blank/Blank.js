1c350a0422a12282f072fd6318b4d325e9c34da3
mmkm10 <mansimadhushree13@gmail.com>
2021-07-01 15:50:34 +0530

    engage

--- Test/client/src/components/Blank/Blank.js
+++ Test/client/src/components/Blank/Blank.js
@@ -16,7 +16,6 @@ function Blank(props) {
         <div className="b">
             <div className="VClogin">
                 <ul><button className="chat-button" onClick={() => history.push('/room')}>Video Meet </button> </ul>
-
                 <ul> <button className="chat-button" onClick={() => alert(Math.random().toString(36).substring(2, 7))}>Chat Room</button></ul>
 
 
